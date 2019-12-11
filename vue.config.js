2164b081fdae17db0b628fa76fc1d3b7c45d3cce
Saikoro2007 <saikorokorokoro20072007@gmail.com>
2019-12-11 04:49:42 +0000

    add built files

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
-    publicPath: "./",
-    assetsDir: ""
+    "publicPath": "./",
+    "assetsDir": "",  
+    "devServer": {
+    "port": 8888,
+    "host": "0.0.0.0",
+    "disableHostCheck": true
+    },
+    "transpileDependencies": [
+    "vuetify"
+    ]
 }
