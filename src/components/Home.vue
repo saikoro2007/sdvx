cc42fbca76f91c1debc9dd569ea503d5c711c7dd
Saikoro2007 <saikorokorokoro20072007@gmail.com>
2019-12-11 08:55:42 +0000

    tmp

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,7 +8,7 @@
             md="4"
           >
             <v-text-field
-              v-model="player1"
+              v-model="playerName1"
               label="Player 1"
               required
             />
@@ -18,7 +18,7 @@
             md="4"
           >
             <v-text-field
-              v-model="player2"
+              v-model="playerName2"
               label="Player 2"
               required
             />
@@ -31,7 +31,7 @@
               color="primary"
               rounded
               outlined
-              required
+              @click="search"
             >
               Compare
             </v-btn>
@@ -39,16 +39,37 @@
         </v-row>
       </v-container>
     </v-form>
+    <v-container>
+      {{ this.playerData1 }}
+    </v-container>
   </v-container>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Home',
 
   data: () => ({
-    player1: '',
-    player2: '',
+    playerName1: '',
+    playerName2: '',
+    playerData1: {},
+    playerData2: {},
   }),
-};
+
+  methods: {
+    async search () {
+      await this.callApi(this.playerName1)
+      await this.callApi(this.playerName2)
+    },
+    callApi (username) {
+      console.log(username)
+      let response = {}
+      axios.get(`https://nearnoah.net/api/showUserData.json?username=${username}`)
+        .then(res => (this.playerData1 = res))
+      return response
+    },
+  }
+}
 </script>
