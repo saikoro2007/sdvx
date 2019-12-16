553775ddf60774339442c5a56ef1d4b69e50cf3b
y-seto <y-seto@infiniteloop.co.jp>
2019-12-16 16:22:09 +0900

    スコアテーブルをいったん別コンポーネントに分離した

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -39,81 +39,28 @@
         </v-row>
       </v-container>
     </v-form>
-    <v-container>
-      <v-data-table
-        v-if="this.playerData1.length > 0"
-        :headers="headers"
-        :items="this.playerData1"
-        :items-per-page="100"
-        :expanded.sync="expanded"
-        color="secondary"
-        show-expand
-        class="elevation-5"
-      >
-        <template v-slot:top>
-          <v-text-field
-            v-model="search"
-            label="Search"
-            append-icon="search"  
-          />
-        </template>
-        <template v-slot:expanded-item="{ headers, item }">
-         <td :colspan="headers.length">
-          <v-list
-            flat
-            dense
-          >
-            <v-list-item-group>
-              <v-list-item v-if="item.novice">
-                NOV(Lv.{{ item.novice.level }}): {{ item.novice.score || 0 }}
-              </v-list-item>
-              <v-list-item v-if="item.advanced">
-                ADV(Lv.{{ item.advanced.level }}): {{ item.advanced.score || 0 }}
-              </v-list-item>
-              <v-list-item v-if="item.exhaust">
-                EXH(Lv.{{ item.exhaust.level }}): {{ item.exhaust.score || 0 }}
-              </v-list-item>
-              <v-list-item v-if="item.maximum">
-                MXM(Lv.{{ item.maximum.level }}): {{ item.maximum.score || 0 }}
-              </v-list-item>
-              <v-list-item v-if="item.infinite">
-                INF(Lv.{{ item.infinite.level }}): {{ item.infinite.score || 0 }}
-              </v-list-item>
-              <v-list-item v-if="item.gravity">
-                GRV(Lv.{{ item.gravity.level }}): {{ item.gravity.score || 0 }}
-              </v-list-item>
-              <v-list-item v-if="item.heavenly">
-                HVN(Lv.{{ item.heavenly.level }}): {{ item.heavenly.score || 0 }}
-              </v-list-item>
-              <v-list-item v-if="item.vivid">
-                VVD(Lv.{{ item.vivid.level }}): {{ item.vivid.score || 0 }}
-              </v-list-item>
-            </v-list-item-group>
-          </v-list>
-         </td>
-        </template>
-      </v-data-table>
-    </v-container>
+    <score-table
+      v-if="this.playerData1.length > 0"
+      :score="playerData1"
+    ></score-table>
   </v-container>
 </template>
 
 <script>
 import axios from 'axios'
+import ScoreTable from './ScoreTable.vue'
 
 export default {
   name: 'Home',
+  components: {
+    ScoreTable: ScoreTable
+  },
 
   data: () => ({
-    expanded: [],
     playerName1: '',
     playerName2: '',
     playerData1: {},
     playerData2: {},
-    search: '',
-    headers: [
-      { text: 'title', value: 'title' },
-      { text: '', value: 'data-table-expand' },
-    ],
   }),
 
   methods: {
---
+++ src/components/ScoreTable.vue
@@ -0,0 +1,73 @@
+<template>
+	<v-container>
+		<v-data-table
+			:headers="headers"
+			:items="this.score"
+			:items-per-page="100"
+			:expanded.sync="expanded"
+			color="secondary"
+			show-expand
+			class="elevation-5"
+		>
+			<template v-slot:top>
+				<v-text-field
+					v-model="search"
+					label="Search"
+					append-icon="search"  
+				/>
+			</template>
+			<template v-slot:expanded-item="{ headers, item }">
+				<td :colspan="headers.length">
+				<v-list
+					flat
+					dense
+				>
+					<v-list-item-group>
+						<v-list-item v-if="item.novice">
+							NOV(Lv.{{ item.novice.level }}): {{ item.novice.score || 0 }}
+						</v-list-item>
+						<v-list-item v-if="item.advanced">
+							ADV(Lv.{{ item.advanced.level }}): {{ item.advanced.score || 0 }}
+						</v-list-item>
+						<v-list-item v-if="item.exhaust">
+							EXH(Lv.{{ item.exhaust.level }}): {{ item.exhaust.score || 0 }}
+						</v-list-item>
+						<v-list-item v-if="item.maximum">
+							MXM(Lv.{{ item.maximum.level }}): {{ item.maximum.score || 0 }}
+						</v-list-item>
+						<v-list-item v-if="item.infinite">
+							INF(Lv.{{ item.infinite.level }}): {{ item.infinite.score || 0 }}
+						</v-list-item>
+						<v-list-item v-if="item.gravity">
+							GRV(Lv.{{ item.gravity.level }}): {{ item.gravity.score || 0 }}
+						</v-list-item>
+						<v-list-item v-if="item.heavenly">
+							HVN(Lv.{{ item.heavenly.level }}): {{ item.heavenly.score || 0 }}
+						</v-list-item>
+						<v-list-item v-if="item.vivid">
+							VVD(Lv.{{ item.vivid.level }}): {{ item.vivid.score || 0 }}
+						</v-list-item>
+					</v-list-item-group>
+				</v-list>
+				</td>
+			</template>
+		</v-data-table>
+	</v-container>
+</template>
+
+<style scoped lang="scss">
+</style>
+
+<script>
+export default {
+	props: ['score'],
+	data: () => ({
+    expanded: [],
+    search: '',
+    headers: [
+      { text: 'title', value: 'title' },
+      { text: '', value: 'data-table-expand' },
+    ],
+	}),
+}
+</script>
